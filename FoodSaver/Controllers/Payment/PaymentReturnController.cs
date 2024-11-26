0b3c2333040440d15267b55238fd3d8d999c213c
rose3085 <rabinasedhai1@gmail.com>
2024-11-26 20:24:35 +0545

    mvc controller for return URL added

---
+++ FoodSaver/Controllers/Payment/PaymentReturnController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodSaver.Controllers.Payment
+{
+    public class PaymentReturnController : Controller
+    {
+      
+
+        public ActionResult ReturnUrl()
+        {
+            return View();
+        }
+    }
+}
